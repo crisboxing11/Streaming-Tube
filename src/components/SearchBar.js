171d59cd7c34d40b949606d2edb581e17408f7b4
crisboxing11 <crisboxing11@gmail.com>
2021-11-20 13:42:37 -0800

    added youtube api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,13 @@ import React from 'react'
 import "../Style/SearchBar.css"
 
 class SearchBar extends React.Component {
-state = {term: 'FUccccck'};
+state = {term: ''};
 
-onFormSubmit = (e) => {
-    e.preventDefault();
+onInputChange = (event) => {
+    this.setState( {term:event.target.value})
+}
+onFormSubmit = event => {
+    event.preventDefault();
     this.props.onSubmit(this.state.term)
 }
 
@@ -17,7 +20,7 @@ onFormSubmit = (e) => {
             <form className="ui form" onSubmit={this.onFormSubmit} type="search">
                 <div className="field">
                     <label>Video Search</label>
-                    <input type="text" placeHolder="Search" value={this.state.term} onChange={e => this.setState({term: e.target.value})}></input>
+                    <input type="text" placeHolder="Search" value={this.state.term} onChange={this.onInputChange}></input>
                 </div>
                 </form>
         </div>
---
+++ src/components/apis/youtube.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+
+
+
+const KEY = 'AIzaSyCv-Ju_3Jb2UAHqTlK_zrgQCEoLw0BWSkU'
+
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY
+    }
+})
