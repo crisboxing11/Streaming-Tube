ab5525ff1291aa994a00bfb809bafb8fa44a78ae
crisboxing11 <crisboxing11@gmail.com>
2021-11-20 14:04:55 -0800

    added state to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import * as React from 'react';
 import SearchBar from './components/SearchBar'
+import youtube from './components/apis/youtube'
 
 class App extends React.Component {
+  state={video: []};
+
+  onTermSubmit = (term) => {
+   const responce = youtube
+   .get('/search', {
+     params: {
+        q: term
+     }
+   })
+  }
 
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onSubmit={this.onTermSubmit} />
       </div>
     );
   }
