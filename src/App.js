8f8d6694e640eebf1dcede00c17339241301c963
crisboxing11 <crisboxing11@gmail.com>
2021-11-20 13:06:01 -0800

    input field added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import SearchBar from './components/SearchBar'
+
+class App extends React.Component {
+
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App
+
+
---
+++ src/components/SearchBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../Style/SearchBar.css"
+
+class SearchBar extends React.Component {
+
+    
+    render () {
+    return (
+        <div className="search-bar ui segment"> 
+            <form className="ui form">
+                <div className="field">
+                    <label>Video Search</label>
+                    <input type="text"></input>
+                </div>
+                </form>
+        </div>
+    )
+    }
+}
+
+export default SearchBar
