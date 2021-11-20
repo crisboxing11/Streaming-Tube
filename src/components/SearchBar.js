9689835821070948577e1458e5362892915a5161
crisboxing11 <crisboxing11@gmail.com>
2021-11-20 13:14:18 -0800

    event handler added to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,15 +2,22 @@ import React from 'react'
 import "../Style/SearchBar.css"
 
 class SearchBar extends React.Component {
+state = {term: 'FUccccck'};
+
+onFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term)
+}
+
 
     
     render () {
     return (
         <div className="search-bar ui segment"> 
-            <form className="ui form">
+            <form className="ui form" onSubmit={this.onFormSubmit} type="search">
                 <div className="field">
                     <label>Video Search</label>
-                    <input type="text"></input>
+                    <input type="text" placeHolder="Search" value={this.state.term} onChange={e => this.setState({term: e.target.value})}></input>
                 </div>
                 </form>
         </div>
