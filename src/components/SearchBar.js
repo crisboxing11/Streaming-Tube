4e9dd859dcc46eff5b1bda86bee565e7835ec890
crisboxing11 <crisboxing11@gmail.com>
2022-01-11 13:32:19 -0800

    SearchBar fully working

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react'
-const SearchBar = (onSubmit) => {
+const SearchBar = ({onSubmit}) => {
     const [term, setTerm] = useState('');
 
-    const onInputChange = (event) => {
-      setTerm(event.target.value);
-    };
+    
     const onFormSubmit = (event) => {
       event.preventDefault();
       onSubmit(term);
@@ -12,14 +10,14 @@ const SearchBar = (onSubmit) => {
      
     return (
       <div className="search-bar ui segment">
-        <form className="ui form" onSubmit={e => onFormSubmit(e)} type="search">
+        <form className="ui form" onSubmit={onFormSubmit} type="search">
           <div className="field">
             <label>Video Search</label>
             <input
               type="text"
               placeholder="Search"
               value={term}
-              onChange={e => onInputChange(e)}
+              onChange={event => setTerm(event.target.value)}
             ></input>
           </div>
         </form>
@@ -28,28 +26,6 @@ const SearchBar = (onSubmit) => {
 
 }
 
-// class SearchBar extends React.Component {
-// state = {term: ''};
 
-// onInputChange = (event) => {
-//     this.setState( {term:event.target.value})
-// }
-// onFormSubmit = event => {
-//     event.preventDefault();
-//     this.props.onSubmit(this.state.term)
-// }
-//     render () {
-//     return (
-//         <div className="search-bar ui segment"> 
-//             <form className="ui form" onSubmit={this.onFormSubmit} type="search">
-//                 <div className="field">
-//                     <label>Video Search</label>
-//                     <input type="text" placeholder="Search" value={this.state.term} onChange={this.onInputChange}></input>
-//                 </div>
-//                 </form>
-//         </div>
-//     )
-//     }
-// }
 
 export default SearchBar
