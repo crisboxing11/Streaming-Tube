7c654906679ea26f9e084ee35a725727e3d06d1e
crisboxing11 <crisboxing11@gmail.com>
2022-01-11 17:26:19 -0800

    Refactoring app component with hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './components/SearchBar'
 import VideoList from './components/VideoList'
 import youtube from './components/apis/youtube'
 import VideoDetail from './components/VideoDetail';
 
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+//  This means to only run the function only one time[]
+  useEffect(() => {
+    onTermSubmit("Dragon Ball Z Abridged");
+  }, [])
+
+
+}
+
+
+
+
+
 class App extends React.Component {
   state={videos: [], selectedVideo: null};
   componentDidMount () {
-    this.onTermSubmit('Super Broly')
+    this.onTermSubmit('Dragon Ball Z Abridged')
   }
 
   onTermSubmit = async (term) => {
