00f2310b3a84a48a107eccb1049faf7949c58b1e
crisboxing11 <crisboxing11@gmail.com>
2022-01-12 18:01:22 -0800

    Updating app component with custom hook

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import useVideos from '../hooks/useVideos';
 const App = () => {
   // Manages the current selected video
   const [selectedVideo, setSelectedVideo] = useState(null);
+
+ const [videos, search] = useVideos('Dragon Ball Z Abridged')
   //  This means to only run the function only one time[]
  
     // setSelectedVideo(response.data.items[0]);
 
   return (
     <div className="ui container">
-      <SearchBar onSubmit={onTermSubmit} />
+      <SearchBar onSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
