8e7b160efacbe75e0c87763c96c35976fea78a9f
crisboxing11 <crisboxing11@gmail.com>
2022-01-12 17:55:45 -0800

    UseVideo component created for custom hooks and reusability

--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from 'react'
+import youtube from '../apis/youtube'
 
-function useVideos() {
-    return (
-        <div>
-            
-        </div>
-    )
+
+const useVideos = (defaultSearchTerm) => {
+  // Manages list of videos
+  const [videos, setVideos] = useState([]);
+  //  This means to only run the function only one time[]
+  useEffect(() => {
+    search(defaultSearchTerm);
+  }, []);
+
+  const search = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    setVideos(response.data.items);
+  };
+
+  return [videos, search]
 }
 
 export default useVideos
